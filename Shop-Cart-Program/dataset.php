a457ec59049cbd67185c81c71e021972b55aa45c
Patrick <cajuguiranp11@gmail.com>
2022-11-25 09:05:16 +0800

    Part1-Program

---
+++ Shop-Cart-Program/dataset.php
@@ -0,0 +1,60 @@
+<?php
+    $arrProducts = array(
+        array(
+            'name'          => "MENS CLASSIC WOBBLY LOGO TEE IN BLACK",
+            'description'   => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste at est magnam temporibus qui asperiores optio quae culpa laudantium ad ipsam, tenetur praesentium porro explicabo. Blanditiis quae eaque mollitia quas quisquam provident maxime? Consequuntur perspiciatis magni eum vitae itaque minima omnis quia ipsam iusto quibusdam, architecto totam libero commodi culpa.",
+            'price'         => "550.75",
+            'photo1'        => "Shirt-A1.jpg",
+            'photo2'        => "Shirt-A2.jpg",
+        ),
+        array(
+            'name'          => "MENS WOBBLY LOGO TEE IN GOLDEN YELLOW",
+            'description'   => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste at est magnam temporibus qui asperiores optio quae culpa laudantium ad ipsam, tenetur praesentium porro explicabo. Blanditiis quae eaque mollitia quas quisquam provident maxime? Consequuntur perspiciatis magni eum vitae itaque minima omnis quia ipsam iusto quibusdam, architecto totam libero commodi culpa.",
+            'price'         => "590.75",
+            'photo1'        => "Shirt-B1.jpg",
+            'photo2'        => "Shirt-B2.jpg",
+        ),
+        array(
+            'name'          => "LEE X SMILEY® MENS TEE IN WHITE",
+            'description'   => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste at est magnam temporibus qui asperiores optio quae culpa laudantium ad ipsam, tenetur praesentium porro explicabo. Blanditiis quae eaque mollitia quas quisquam provident maxime? Consequuntur perspiciatis magni eum vitae itaque minima omnis quia ipsam iusto quibusdam, architecto totam libero commodi culpa.",
+            'price'         => "699.75",
+            'photo1'        => "Shirt-C1.jpg",
+            'photo2'        => "Shirt-C2.jpg",
+        ),
+        array(
+            'name'          => "MENS CLASSIC LOGO TEE IN ATLANTIC BLUE",
+            'description'   => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste at est magnam temporibus qui asperiores optio quae culpa laudantium ad ipsam, tenetur praesentium porro explicabo. Blanditiis quae eaque mollitia quas quisquam provident maxime? Consequuntur perspiciatis magni eum vitae itaque minima omnis quia ipsam iusto quibusdam, architecto totam libero commodi culpa.",
+            'price'         => "690.75",
+            'photo1'        => "Shirt-D2.png",
+            'photo2'        => "Shirt-D1.png",
+        ),
+        array(
+            'name'          => "MENS BROOKLYN STRAIGHT KNIT KHAKI PANTS",
+            'description'   => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste at est magnam temporibus qui asperiores optio quae culpa laudantium ad ipsam, tenetur praesentium porro explicabo. Blanditiis quae eaque mollitia quas quisquam provident maxime? Consequuntur perspiciatis magni eum vitae itaque minima omnis quia ipsam iusto quibusdam, architecto totam libero commodi culpa.",
+            'price'         => "890.00",
+            'photo1'        => "Jean-A1.JPG",
+            'photo2'        => "Jean-A2.JPG",
+        ),
+        array(
+            'name'          => "MENS BROOKLYN STRAIGHT DARK NAVY PANTS",
+            'description'   => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste at est magnam temporibus qui asperiores optio quae culpa laudantium ad ipsam, tenetur praesentium porro explicabo. Blanditiis quae eaque mollitia quas quisquam provident maxime? Consequuntur perspiciatis magni eum vitae itaque minima omnis quia ipsam iusto quibusdam, architecto totam libero commodi culpa.",
+            'price'         => "890.00",
+            'photo1'        => "Jean-B1.JPG",
+            'photo2'        => "Jean-B2.JPG",
+        ),
+        array(
+            'name'          => "LEE 101+ MENS LUKE DENIM BLUE BLACK",
+            'description'   => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste at est magnam temporibus qui asperiores optio quae culpa laudantium ad ipsam, tenetur praesentium porro explicabo. Blanditiis quae eaque mollitia quas quisquam provident maxime? Consequuntur perspiciatis magni eum vitae itaque minima omnis quia ipsam iusto quibusdam, architecto totam libero commodi culpa.",
+            'price'         => "990.00",
+            'photo1'        => "Jean-C1.jpg",
+            'photo2'        => "Jean-C2.jpg",
+        ), 
+        array(
+            'name'          => "MENS MALONE DENIM JEANS INSEAM IN BLACK",
+            'description'   => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste at est magnam temporibus qui asperiores optio quae culpa laudantium ad ipsam, tenetur praesentium porro explicabo. Blanditiis quae eaque mollitia quas quisquam provident maxime? Consequuntur perspiciatis magni eum vitae itaque minima omnis quia ipsam iusto quibusdam, architecto totam libero commodi culpa.",
+            'price'         => "990.00",
+            'photo1'        => "Jean-D1.JPG",
+            'photo2'        => "Jean-D2.JPG",
+        )
+    );
+?>
